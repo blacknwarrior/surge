9928b3f6df6f88d5e2631aaf789d36a6f55682cd
blacknwarrior <107193123+blacknwarrior@users.noreply.github.com>
2023-06-08 14:24:12 +0800

    Update youjia.js

--- scripts/youjia.js
+++ scripts/youjia.js
@@ -88,11 +88,14 @@ $httpClient.get(
 
             const friendly_tips = `${adjust_date} ${adjust_trend} ${adjust_value}`
 
-      if (prices.length !== 4) {
-        console.log( `解析油价信息失败, 数量=${prices.length},  URL=${query_addr}`);
-        done();
-      } else {
-        $done($notification.post("实时油价信息", `${friendly_tips}`, `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}\n${prices[3].name}  ${prices[3].value}`, "https://google.com"));
+            if (prices.length !== 4) {
+                console.log(`解析油价信息失败, 数量=${prices.length}, 请反馈至 @RS0485: URL=${query_addr}`)
+                done({})
+            }
+            else {
+                body = {
+                    title: "实时油价信息",
+                    content: `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}\n${prices[3].name}  ${prices[3].value}\n${friendly_tips}`,
                     icon: "fuelpump.fill"
                 }
 
