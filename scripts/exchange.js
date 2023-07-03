12a6060f57bd7cf91f8bfc9e961f67e72048bde7
blacknwarrior <107193123+blacknwarrior@users.noreply.github.com>
2023-07-03 14:05:09 +0800

    Update exchange.js

--- scripts/exchange.js
+++ scripts/exchange.js
@@ -38,11 +38,11 @@ $.http.get({url: "https://api.fer.ee/latest?base=USD"})
                 const rate = parseFloat(data.rates[key]);
                 const target = currencyNames[key];
                 if (rate > 1) {
-                    line = `${target[]} 1${source[0]}/${roundNumber(rate, digits)}${
+                    line = `${target[1]} 1${source[0]}/${roundNumber(rate, digits)}${
                         target[0]
                     }\n`;
                 } else {
-                    line = `${target[]} 1${source[0]}/${roundNumber(rate, digits)}${
+                    line = `${target[1]} 1${source[0]}/${roundNumber(rate, digits)}${
                         target[0]
                     }\n`;
                 }
