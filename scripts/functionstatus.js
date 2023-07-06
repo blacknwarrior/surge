e809f0d4b2a42f9f3c99ef2f65edde9e2ca0e784
blacknwarrior <107193123+blacknwarrior@users.noreply.github.com>
2023-07-06 23:41:10 +0800

    Update functionstatus.js

--- scripts/functionstatus.js
+++ scripts/functionstatus.js
@@ -48,7 +48,7 @@ if(days==0){
     }
     return(`${hours}hr${minutes}min${seconds}sec`)
   }else {
-        return(`${days}de${hours}min${minutes}sec`)
+        return(`${days}dd${hours}min${minutes}sec`)
 	}
 }
 function httpAPI(path = "", method = "POST", body = null) {
