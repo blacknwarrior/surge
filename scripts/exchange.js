245239ea38fbe4c5c13abc6866c4bd38cd56b9f2
blacknwarrior <107193123+blacknwarrior@users.noreply.github.com>
2023-09-08 11:15:08 +0800

    Update exchange.js

--- scripts/exchange.js
+++ scripts/exchange.js
@@ -18,8 +18,8 @@ const digits = 4; // 保留几位有效数字
 
 const $ = API("exchange");
 const currencyNames = {
-    USD: ["U.S.Dollar", "🇺🇸"], //美元
-    CNY: ["Chinese Yuan", "🇨🇳"], //人民币
+    USD: ["USD", "🇺🇸"], //美元
+    CNY: ["CNY", "🇨🇳"], //人民币
     HKD: ["HKD", "🇭🇰"], //港币
     EUR: ["EUR", "🇪🇺"], //欧元
     GBP: ["GBP", "🇬🇧"], //英镑
